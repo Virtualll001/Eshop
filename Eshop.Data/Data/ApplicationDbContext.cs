6e9d69c787af7cdeb1afb45607a1b4e12e26e683
Virtualll001 <virtualll001@gmail.com>
2021-03-01 14:58:04 +0100

    Lekce1a2
    Skončeno add-migration identity

---
+++ Eshop.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Eshop.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eshop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
